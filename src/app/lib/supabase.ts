443d75a1ee2d24d8071982f7b8e8f48db410192b
PrzemyslawBazan <p.bazan@taxm.pl>
2026-05-14 10:29:03 +0200

    persist-session

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -5,7 +5,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    persistSession: true,
+    persistSession: false,
       autoRefreshToken: true,
       detectSessionInUrl: true,
       storage: {
